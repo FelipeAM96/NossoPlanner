5da1017c6b074ff3068d5a988e4222147e51ffca
Felipe Aguiar Martin <131861339+FelipeAM96@users.noreply.github.com>
2026-07-10 16:34:22 -0300

    Update cache name and add logo image to cache

--- sw.js
+++ sw.js
@@ -1,11 +1,10 @@
-const CACHE_NAME = 'planner-cache-v1';
+const CACHE_NAME = 'planner-cache-v1.1';
 
-// O que baixar para o celular assim que a pessoa abrir a primeira vez
 const urlsToCache = [
     './',
     './index.html',
     './manifest.json',
-    'https://cdn-icons-png.flaticon.com/512/2693/2693507.png'
+    './img/Logo_Planner.png'
 ];
 
 self.addEventListener('install', event => {
@@ -17,12 +16,11 @@ self.addEventListener('install', event => {
     );
 });
 
-// Quando o celular pedir um arquivo, entrega do Cache se não tiver internet
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => {
-                // Se achou no cache, entrega o cache. Se não, tenta baixar da rede.
+                // Se achou no cache, entrega. Se não, busca na rede.
                 return response || fetch(event.request);
             })
     );
